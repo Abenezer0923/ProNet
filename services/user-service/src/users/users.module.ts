d235d4f17ee1cf28e53541b5bee1d5a6a1a6a1cc
Abenezer <abenezergetachew1990@gmail.com>
2025-11-19 15:48:51 +0300

    fix: Register Experience and Education entities in UsersModule

--- services/user-service/src/users/users.module.ts
+++ services/user-service/src/users/users.module.ts
@@ -5,11 +5,13 @@ import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { UserSkill } from './entities/user-skill.entity';
 import { Connection } from './entities/connection.entity';
+import { Experience } from './entities/experience.entity';
+import { Education } from './entities/education.entity';
 import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, UserSkill, Connection]),
+    TypeOrmModule.forFeature([User, UserSkill, Connection, Experience, Education]),
     NotificationsModule,
   ],
   controllers: [UsersController],
